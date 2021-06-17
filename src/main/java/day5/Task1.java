f4bd24f7fa42fb897fb96ba4d8beac53cc26b77a
Dmitriy Tereshin <d.tereshin202@gmail.com>
2021-06-17 20:02:15 +0300

    complete day5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,14 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car = new Car();
 
+        car.setYear(2010);
+        car.setModel("Ford Focus");
+        car.setColor("Red");
+
+        System.out.println("Год выпуска машины: " + car.getYear());
+        System.out.println("Модель машины: " + car.getModel());
+        System.out.println("Цвет машины: " + car.getColor());
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,10 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorcycle motorcycle = new Motorcycle(2015, "Honda Gold Wing", "Black");
 
+        System.out.println("Год выпуска мотоцикла: " + motorcycle.getYear());
+        System.out.println("Модель мотоцикла: " + motorcycle.getModel());
+        System.out.println("Цвет мотоцикла: " + motorcycle.getColor());
     }
 }
