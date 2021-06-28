c528377d08914914bb5a8650161d640154a56dff
Dmitriy Tereshin <d.tereshin202@gmail.com>
2021-06-28 13:55:40 +0300

    complete day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,24 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        long time = System.currentTimeMillis();
+        String line = "";
 
+        for (int i = 0; i <= 20000; i++) {
+            line += (i + " ");
+        }
+
+        System.out.println((System.currentTimeMillis() - time) + " милисекунд");
+        System.out.println(line);
+
+        long time2 = System.currentTimeMillis();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i <= 20000; i++) {
+            stringBuilder.append((i + " "));
+        }
+
+        System.out.println((System.currentTimeMillis() - time2) + " милисекунд");
+        System.out.println(stringBuilder);
     }
 }
